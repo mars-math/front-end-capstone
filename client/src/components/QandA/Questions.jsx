86ede1c167f327ef6ae387402ae0852c19d9294d
Rockwell Allen <rockwellallen@gmail.com>
2022-02-10 15:18:27 -0800

    added potential basic structure for Q&A

---
+++ client/src/components/QandA/Questions.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import QuestionsView from './QuestionsView.jsx';
+
+function Questions() {
+
+  return (
+    <>
+      <div>
+        <QuestionsView />
+      </div>
+    </>
+  );
+
+}
+
+
+
+
+
+
+
+
+export default Questions;
