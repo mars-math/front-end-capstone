8a3b9c8f455d17801fc96563e6341aee117a3372
Alex Hu <alexhumeow@gmail.com>
2022-02-09 17:55:33 -0800

    test2

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,9 +10,9 @@ class App extends React.Component {
     return (
       <>
         <div>something else</div>
-        <div>related</div>
-        <div>QA</div>
-        <div>RR</div>
+        <div>one</div>
+        <div>two</div>
+        <div>three</div>
       </>
     );
   }
