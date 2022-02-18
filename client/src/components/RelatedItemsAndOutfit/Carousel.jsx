362208d70ac5a797c281d466a064762cc965e692
mtanaka <matthew.s.tanaka@gmail.com>
2022-02-17 16:29:18 -0800

    render Carousel to DOM

---
+++ client/src/components/RelatedItemsAndOutfit/Carousel.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/extensions */
+
+import React from 'react';
+import ProductCard from './ProductCard.jsx';
+import SampleData from './sampledata/sampledata.js';
+
+function Carousel() {
+  // take in product ID
+  return (
+    // SampleData.relatedIDs.map((id) => <ProductCard prodId={id} key={id} />)
+    <div>hello</div>
+  );
+  // return (<div>Hi from the Carousel component</div>);
+}
+
+export default Carousel;
