1bc00e8691e172065250d2a51178f20ef0edf984
sarahma123 <94800595+sarahma123@users.noreply.github.com>
2022-02-18 19:31:03 -0800

    Sm static all (#34)
    * Starting style selector
    
    * Style Selector renders with satic data, prints rows of 4
    
    * Static rendering of all parts (mostly) + arrangement via flexbox
    
    * Added description and resized main image
    
    * Changed component orders in App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import QandA from './QandA/QandA.jsx';
 import AllReviews from './RatingsReview/AllReviews.jsx';
-import ProductInfo from './Overview/SM-ProductInfo.jsx';
+import Overview from './Overview/SM-Overview.jsx';
 import Carousel from './RelatedItemsAndOutfit/Carousel.jsx';
 import RatingBreakdown from './RatingsReview/RatingBreakdown.jsx';
 
@@ -13,17 +13,25 @@ class App extends React.Component {
   render() {
     return (
       <>
-      <RatingBreakdown />
-        {/* <h1>Kloth</h1>
-        <ProductInfo />
+        <h1>Kloth</h1>
+        <Overview />
+        <RatingBreakdown />
         <Carousel renderedId={'42368'}/>
+        <Carousel />
         <div>
           <QandA />
         </div>
-        <AllReviews /> */}
+        <AllReviews />
       </>
     );
   }
 }
 
 export default App;
+
+/*
+1. Overview
+2. Reviews
+3. Q&A
+4. Related/Outfit
+*/
