8e1c1270f0661565bfb1027cb379123ae46de4d4
mtanaka <matthew.s.tanaka@gmail.com>
2022-02-18 21:57:17 -0800

    began creating outfitlist component

--- client/src/components/RelatedItemsAndOutfit/Carousel.jsx
+++ client/src/components/RelatedItemsAndOutfit/Carousel.jsx
@@ -2,32 +2,9 @@
 
 import React from 'react';
 import ProductCard from './ProductCard.jsx';
-// import axios from 'axios';
-// import API_KEY from '../../../../config/config.js';
 
 function Carousel(props) {
-  // take in product ID from App.jsx as a prop
   const { renderedIds } = props;
-  // const [relatedIds, setRelatedIds] = useState([]);
-
-  // state for list of all related items from a product ID
-  // state for index
-
-  // const getRelatedIds = (id) => {
-  //   axios.defaults.baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax';
-  //   axios.defaults.headers.common.Authorization = API_KEY;
-  //   return (
-  //     axios.get(`/products/${id}/related`)
-  //       .then((response) => {
-  //         setRelatedIds(response.data);
-  //       })
-  //       .catch((err) => console.log('MT error: ', err))
-  //   );
-  // };
-
-  // useEffect(() => {
-  //   getRelatedIds(renderedId);
-  // }, []);
 
   return (
     <>
---
+++ client/src/components/RelatedItemsAndOutfit/OutfitList.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable import/extensions */
+
+import React, { useEffect, useState } from 'react';
+import Carousel from './Carousel.jsx';
+
+function OutfitList(props) {
+  // take in product ID from App.jsx as a prop
+  const { renderedId } = props;
+  const [relatedIds, setRelatedIds] = useState([]);
+
+  // state for list of all related items from a product ID
+  // state for index
+
+  const getRelatedIds = (id) => {
+    axios.defaults.baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax';
+    axios.defaults.headers.common.Authorization = API_KEY;
+    return (
+      axios.get(`/products/${id}/related`)
+        .then((response) => {
+          setRelatedIds(response.data);
+        })
+        .catch((err) => console.log('MT error: ', err))
+    );
+  };
+
+  useEffect(() => {
+    getRelatedIds(renderedId);
+  }, []);
+
+  return (
+    <>
+      <h3>Related Items</h3>
+      <Carousel renderedIds={relatedIds} />
+    </>
+  );
+}
+
+export default OutfitList;
