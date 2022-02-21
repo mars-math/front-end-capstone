4b9b70970c40c6a081c64752d66d3bec0ea5f596
Rockwell Allen <rockwellallen@gmail.com>
2022-02-20 20:04:10 -0800

    helpful click on answers complete

--- client/src/components/QandA/AnswersView.jsx
+++ client/src/components/QandA/AnswersView.jsx
@@ -6,57 +6,56 @@ class AnswersView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clickedHelpful: true,
+      clickedHelpfulA: true,
     };
 
-    // this.clickHelpfulA = this.clickHelpfulA.bind(this);
-    // this.helpfulACounterDisplay = this.helpfulACounterDisplay.bind(this);
+    this.clickHelpfulA = this.clickHelpfulA.bind(this);
+    this.helpfulACounterDisplay = this.helpfulACounterDisplay.bind(this);
   }
 
-  // clickHelpfulA() {
-  //   const qID = this.props.questions.question_id;
-  //   if (this.state.clickedHelpful) {
-  //     axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions/${qID}/helpful`, {
-  //       headers: {
-  //         Authorization: API_KEY,
-  //       },
-  //     })
-  //     .then((results) => {
-  //       console.log('results ', results);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  //     this.setState({ clickedHelpful: !this.state.clickedHelpful });
-  //   }
-  // }
+  clickHelpfulA() {
+    const aID = this.props.answer.id;
+    if (this.state.clickedHelpfulA) {
+      axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/answers/${aID}/helpful`, {
+        headers: {
+          Authorization: API_KEY,
+        },
+      })
+      .then((results) => {
+        console.log('results ', results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      this.setState({ clickedHelpfulA: !this.state.clickedHelpfulA });
+    }
+  }
 
-  // helpfulACounterDisplay() {
-  //   if (this.state.clickedHelpful) {
-  //     return (<>{ this.props.questions.question_helpfulness }</>);
-  //   } else {
-  //     return (<>{ this.props.questions.question_helpfulness + 1}</>);
-  //   }
-  // }
+  helpfulACounterDisplay() {
+    if (this.state.clickedHelpfulA) {
+      return (<>{ this.props.answer.helpfulness }</>);
+    } else {
+      return (<>{ this.props.answer.helpfulness + 1 }</>);
+    }
+  }
 
   render() {
     return (
       <>
+        {console.log('answer ', this.props.answer)}
         <span class='answer-list'>{this.props.answer.body}</span>
         <div class='answer-footer'>
           <span>by {this.props.answer.answerer_name}</span>
           <span>    date {new Date(this.props.answer.date).toLocaleString().split(",")[0]} |</span>
           <span>    Helpful?  </span>
-          <span><u>Yes</u> </span>
-          <span>({this.props.answer.helpfulness})</span>
+          <span onClick={this.clickHelpfulA}><u>Yes</u> </span>
+          <span>({this.helpfulACounterDisplay()})</span>
           <span>     |  <u>Report</u> </span>
         </div>
       </>
     );
   }
-
 }
 
 export default AnswersView;
-// onClick={this.clickHelpfulA}
 // {console.log('answers in AV ', this.props.answer)}
