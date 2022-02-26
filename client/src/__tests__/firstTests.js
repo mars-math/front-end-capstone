824fdb722b66613349e666082bd1568e354dd819
sarahma123 <94800595+sarahma123@users.noreply.github.com>
2022-02-25 16:58:31 -0800

    Sm last styling (#65)
    * starting jest server mock
    
    * committing to switch branches
    
    * Styling version from presentation

--- client/src/__tests__/firstTests.js
+++ client/src/__tests__/firstTests.js
@@ -6,23 +6,11 @@ import App from './components/App.jsx';
 import Price from './components/Overview/SM-Price.jsx';
 import '@testing-library/jest-dom';
 
-// dummy test
-// it('one is one', () => {
-//   expect(1).toEqual(1)
-// });
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-});
-
-it('renders correctly', () => {
-  const tree = renderer
-  .create(<App url='43230'/>)
-  .toJSON();
-  console.log(tree)
-  expect(tree).toMatchSnapshot();
 });
 
 test('it renders given category name', () => {
@@ -37,3 +25,14 @@ it('renders correctly', () => {
   expect(tree).toMatchSnapshot();
 });
 
+describe('testMock', () => {
+  afterEatch(() => jest.resetAllMocks());
+
+  it('test test test', async () => {
+    const dummyData = await getData(42366);
+    expect(axios.request).toHaveBeenCalled();
+    expect(axios.request).toHaveBeenCalledWith({ method: 'get', url: '/42366' });
+    expect(photos.length).toEqual(1);
+    expect(photos[0].albumId).toEqual(42366);
+  })
+})
