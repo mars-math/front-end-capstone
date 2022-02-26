469a211bf108d5de2ae4794ed9fe535ebe3ba3b4
sarahma123 <94800595+sarahma123@users.noreply.github.com>
2022-02-25 16:30:06 -0800

    starting jest server mock

--- client/src/__tests__/firstTests.js
+++ client/src/__tests__/firstTests.js
@@ -6,23 +6,11 @@ import App from './components/App.jsx';
 import Price from './components/Overview/SM-Price.jsx';
 import '@testing-library/jest-dom';
 
-// dummy test
-// it('one is one', () => {
-//   expect(1).toEqual(1)
-// });
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-});
-
-it('renders correctly', () => {
-  const tree = renderer
-  .create(<App url='43230'/>)
-  .toJSON();
-  console.log(tree)
-  expect(tree).toMatchSnapshot();
 });
 
 test('it renders given category name', () => {
